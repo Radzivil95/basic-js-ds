0bc2248ed2d02c3d5736e2d940c8fb84dcc99252
VladRadzivil <vladradziviloff@gmail.com>
2022-10-17 14:58:53 +0300

    feat: added solution queue

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
+ class Queue {
+  constructor() {
+    this.queue = [];
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let list = null;
+    for (let i = this.queue.length - 1; i >= 0; i--) {
+      list = {
+        value: this.queue[i],
+        next: list
+      };
+    }
+    return list;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    this.queue.push(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.queue.shift();
   }
 }
 
